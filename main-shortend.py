46cc2b7f819c3ff18db91e15ac587d016f623bc1
parthasarathisantra <parthasarathisantra227@gmail.com>
2025-11-20 16:51:50 +0530

    short analysis

---
+++ main-shortend.py
@@ -0,0 +1,45 @@
+# game repo
+import random
+'''
+1  for snake
+-1 for water
+0 for gun
+'''
+computer  = random.choice([-1, 1, 0])
+youstr = input("Enter your Choice: ")
+youdict = {"s": 1, "w": -1, "g": 0}
+reverdict = {1: "Snake", -1: "Water", 0: "Gun"}
+you = youdict[youstr]
+
+print(f"You Choose {reverdict[you]}\nComputer Choose {reverdict[computer]}")
+
+if(computer == you):
+    print("its a draw")
+
+else:
+   
+    """if(computer == -1 and you == 1):(computer - you) = -2
+       
+       print("You Win")
+
+    elif(computer == -1 and you == 0):(computer - you) = -1
+        print("You Lose")
+
+    elif(computer == 1 and you == -1):(computer - you) =  2
+        print("You Lose")
+
+    elif(computer == 1 and you == 0):(computer - you) =   1
+        print("You Win")
+
+    elif(computer == 0 and you == -1):(computer - you) =   1
+        print("You Win")
+
+    elif(computer == 0 and you == 1):(computer - you) =    -1
+        print("You Lose")
+
+    else:
+        print("Something Went wrong")"""
+    if((computer - you) == -1 or (computer - you) == 2):
+        print("You Loose")
+    else:
+        print("You Win")
